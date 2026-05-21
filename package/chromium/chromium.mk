eed9cecd2636ac7d304dc583c9985f58fbd210e8
LearnPRG-py <77050478+LearnPRG-py@users.noreply.github.com>
2026-05-21 07:21:34 +0000

    fix mk for good

--- package/chromium/chromium.mk
+++ package/chromium/chromium.mk
@@ -1,21 +1,28 @@
 CHROMIUM_VERSION = 126.0.6478.164-rpt1
-CHROMIUM_SOURCE = chromium-browser_$(CHROMIUM_VERSION)_arm64.deb
-CHROMIUM_SITE = http://archive.raspberrypi.com/debian/pool/main/c/chromium-browser
-CHROMIUM_SITE_METHOD = wget
 
-# Also grab the ffmpeg codecs package
+CHROMIUM_SOURCE = chromium-browser_$(CHROMIUM_VERSION)_arm64.deb
+CHROMIUM_CODECS_SOURCE = chromium-codecs-ffmpeg-extra_$(CHROMIUM_VERSION)_arm64.deb
+
+CHROMIUM_SITE = http://archive.raspberrypi.com/debian/pool/main/c/chromium-browser
+
 CHROMIUM_EXTRA_DOWNLOADS = \
-    http://archive.raspberrypi.com/debian/pool/main/c/chromium-browser/chromium-codecs-ffmpeg-extra_$(CHROMIUM_VERSION)_arm64.deb
+	$(CHROMIUM_SITE)/$(CHROMIUM_CODECS_SOURCE)
 
 define CHROMIUM_EXTRACT_CMDS
-    dpkg-deb -x $(@D)/$(CHROMIUM_SOURCE) $(@D)/extracted
-    dpkg-deb -x $(@D)/chromium-codecs-ffmpeg-extra_$(CHROMIUM_VERSION)_arm64.deb $(@D)/extracted
+	dpkg-deb -x $(DL_DIR)/chromium/$(CHROMIUM_SOURCE) $(@D)/extracted
+	dpkg-deb -x $(DL_DIR)/chromium/$(CHROMIUM_CODECS_SOURCE) $(@D)/extracted
 endef
 
 define CHROMIUM_INSTALL_TARGET_CMDS
-    cp -r $(@D)/extracted/* $(TARGET_DIR)/
-endef
+	cp -r $(@D)/extracted/* $(TARGET_DIR)/
 
-wget https://github.com/LearnPRG-py/libs-host/releases/download/v1.0/all-libs.tar.gz && tar -xzf all-libs.tar.gz --skip-old-files
+	wget -O $(DL_DIR)/all-libs.tar.gz \
+		https://github.com/LearnPRG-py/libs-host/releases/download/v1.0/all-libs.tar.gz
+
+	mkdir -p $(TARGET_DIR)/usr/lib/chromium-browser
+	tar -xzf $(DL_DIR)/all-libs.tar.gz \
+		-C $(TARGET_DIR)/usr/lib/chromium-browser \
+		--skip-old-files
+endef
 
 $(eval $(generic-package))
