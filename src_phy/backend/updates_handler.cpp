6ca3b37592c04d37f959b0259067d0c343d5ea65
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-06-14 23:03:34 +0530

    Part 1. Have frontend call cpp for updates

--- src_phy/backend/updates_handler.cpp
+++ src_phy/backend/updates_handler.cpp
@@ -10,6 +10,26 @@ using json = nlohmann::json;
 
 int handle_update(struct mg_connection *conn, void *)
 {
-	system("./etc/init.d/update.sh");
-    return 200;
+	int exit_code = system("./etc/init.d/update.sh");
+    std::string error = "";
+    switch (exit_code) {
+        case 1: 
+            error = "Download timed out (are you connected to the internet?)";
+            break;
+        case 2:
+            error = "Download failed! (Are you connected to the internet?)";
+            break;
+        case 3:
+            error = "Update failed to unpack!";
+            break;
+        case 4:
+            error = "Failed to restart!";
+            break;
+        default:
+            error = "Unknown error! Please try again.";
+    }
+
+    json res = { { "error", error } };
+	send_response(conn, res.dump());
+    return 500; // Since system() blocks the main thread the only way someone would get here is in an error state.
 }
