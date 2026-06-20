89ba30de27b3ce1797c227d28c34c161c539a02b
Aryan <aryankrishnan4b@gmail.com>
2026-06-20 12:26:16 +0530

    Skip auth for now before we move to S3

--- board/raspberrypi/rootfs-overlay/etc/init.d/update.sh
+++ board/raspberrypi/rootfs-overlay/etc/init.d/update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Step 1: Curl the release URL:
-curl --max-time 30 -o /tmp/release.json https://api.github.com/repos/raviishh/SuvriddhiOS/releases/latest
+curl --max-time 30 -k -o /tmp/release.json https://api.github.com/repos/raviishh/SuvriddhiOS/releases/latest
 DOWNLOAD_URL=$(grep -o '"browser_download_url": *"[^"]*"' /tmp/release.json | grep -o 'https://[^"]*')
-curl --max-time 60 -L -o /tmp/suvriddhi.zip "$DOWNLOAD_URL"
+curl --max-time 60 -L -k -o /tmp/suvriddhi.zip "$DOWNLOAD_URL"
 
 # Step 2: After downloading you have a zip file, create /root/www_new with the extracted contents of the zip
 mkdir -p /root/www_new
