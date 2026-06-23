d44148d16a61bc667665f571aadb33888c26eb3c
Aryan <aryankrishnan4b@gmail.com>
2026-06-23 19:05:55 +0530

    format

--- src_cs/backend/code/compile_handler.cpp
+++ src_cs/backend/code/compile_handler.cpp
@@ -12,11 +12,11 @@ int HandleCompile(struct mg_connection *conn, void *)
 	std::string path_root = "/tmp/" + token;
 	WriteFile(path_root + ".c", code);
 
-	std::string compileCmd = "gcc -std=c11 -O2 " + path_root + ".c -o " + path_root + " 2>"+path_root+ ".log";
+	std::string compileCmd = "gcc -std=c11 -O2 " + path_root + ".c -o " + path_root + " 2>" + path_root + ".log";
 	int ret = std::system(compileCmd.c_str());
 
 	json res;
-	res["token"] = (ret = 0) ?  token : nullptr;
+	res["token"] = (ret = 0) ? token : nullptr;
 	res["error"] = (ret = 0) ? nullptr : ReadFile(path_root + ".log");
 	SendResponse(conn, res.dump());
 	return 200;
