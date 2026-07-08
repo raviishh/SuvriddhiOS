e34bfc9761bf4750e0e9ef0df6d9ea9d9180d511
Aryan <aryankrishnan4b@gmail.com>
2026-07-08 23:09:01 +0530

    Fix

--- src_cs/backend/settings/power_handler.cpp
+++ src_cs/backend/settings/power_handler.cpp
@@ -7,10 +7,10 @@ int handle_power(struct mg_connection *conn, void *)
 	json req = GetJsonReq(conn);
 	std::string command = req["cmd"];
 	if (command == "restart") {
-		int ret = system("reboot &");
+		int ret = system("(/etc/init.d/S60cage stop && sleep 1 && reboot) > /dev/null 2>&1 &");
 		std::cerr << "system(\"reboot\") returned: " << ret << std::endl;
 	} else if (command == "shutdown") {
-		int ret = system("poweroff &");
+		int ret = system("(/etc/init.d/S60cage stop && sleep 1 && poweroff) > /dev/null 2>&1 &");
 		std::cerr << "system(\"poweroff\") returned: " << ret << std::endl;
 	} else if (command == "sleep") {
 		// TODO: add support for sleep at some point.
